831dba3803cf0a0cb27c6c0364df17ca543e5a71
huayiqiu <284527552@qq.com>
2017-12-21 20:29:02 +0800

    2017.12.21 add sample

--- shell.py
+++ shell.py
@@ -1,79 +1,9 @@
-import paramiko,sys,os,select
+import paramiko, sys, os, socket, select, getpass, termios, tty
+from paramiko.py3compat import u
 
-Hosts = {
-    '10.0.0.3':1212,
-    '10.0.0.4':1212,
-
-}
-username = 'admin'
-password = '123456'
-port=1212
-while True:
-    print("Welcome!")
-    user = input("input username:").strip()
-    psd = input("input password:").strip()
-    if user == username and psd == password:
-        while True:
-            host_list=[]
-            for key,host in enumerate(Hosts.keys(),1):
-                print(key,host)
-                host_list.append((key,host))
-            inp = int(input("input:"))
-            print("Hosts List:")
-            for num in host_list:
-                if inp == num[0]:
-                    print(num[1])
-                    print("port:",Hosts[num[1]])
-                    break
-        else:
-            pass
-    else:
-        print("not match!")
-    break
-else:
-    pass
-
-class WindosShell(object):
-
-    def __init__(self,host,port):
-        self.host = host
-        self.port = port
-        # self.username = 'root'
-        # self.password = 'huayiqiu'
-        self.tran = paramiko.Transport((self.host, self.port))
-        self.tran.connect(username="root", password='huayiqiu')
-        self.ssh = paramiko.SSHClient()
-        self.ssh._transport = self.tran
-        # 打开一个通道
-        chan = self.tran.open_session()
-        # 获取一个终端
-        chan.get_pty()
-        # 激活器
-        chan.invoke_shell()
-    def listen(self):
-        while True:
-            # 监视用户输入和服务器返回数据
-            # sys.stdin 处理用户输入
-            # chan 是之前创建的通道，用于接收服务器返回信息
-            readable, writeable, error = select.select([self.chan, sys.stdin, ], [], [], 1)
-            if self.chan in readable:
-                try:
-                    x = self.chan.recv(1024)
-                    if len(x) == 0:
-                        print('\r\n*** EOF\r\n',)
-                        break
-                    sys.stdout.write(x)
-                    sys.stdout.flush()
-                except self.socket.timeout:
-                    pass
-            if sys.stdin in readable:
-                inp = sys.stdin.readline()
-                self.chan.sendall(inp)
-"""
-tran = paramiko.Transport(('10.0.0.3', 1212))
-tran.connect(username="root",password='huayiqiu')
-ssh = paramiko.SSHClient()
-ssh._transport = tran
+tran = paramiko.Transport(('10.0.0.3', 1212,))
+tran.start_client()
+tran.auth_password('root', 'huayiqiu')
 # 打开一个通道
 chan = tran.open_session()
 # 获取一个终端
@@ -81,41 +11,41 @@ chan.get_pty()
 # 激活器
 chan.invoke_shell()
 
-#########
-# 利用sys.stdin,肆意妄为执行操作
-# 用户在终端输入内容，并将内容发送至远程服务器
-# 远程服务器执行命令，并将结果返回
-# 用户终端显示内容
-#########
+# 获取原tty属性
+oldtty = termios.tcgetattr(sys.stdin)
+try:
+    # 为tty设置新属性
+    # 默认当前tty设备属性：
+    #   输入一行回车，执行
+    #   CTRL+C 进程退出，遇到特殊字符，特殊处理。
+    # 这是为原始模式，不认识所有特殊符号
+    # 放置特殊字符应用在当前终端，如此设置，将所有的用户输入均发送到远程服务器
+    tty.setraw(sys.stdin.fileno())  # 恢复终端原始状态，每按一个键就发送
+    chan.settimeout(0.0)
 
-def windows_shell(chan):
-    import threading
-
-    print("Line-buffered terminal emulation. Press F6 or ^Z to send EOF.")
-
-    def writeall(sock):
-        while True:
-            data = sock.recv(256)
-            if not data:
-                print('*** EOF ***')
+    while True:
+        # 监视 用户输入 和 远程服务器返回数据（socket）
+        # 阻塞，直到句柄可读
+        r, w, e = select.select([chan, sys.stdin], [], [], 1)
+        if chan in r:  # 获取服务返回的内容
+            try:
+                x = u(chan.recv(1024))
+                if len(x) == 0:
+                    print('\r\n*** EOF\r\n')
+                    break
+                sys.stdout.write(x)
+                sys.stdout.flush()
+            except socket.timeout:
+                pass
+        if sys.stdin in r:  # 发送命令
+            x = sys.stdin.read(1)  # 读取一个字符
+            if len(x) == 0:
                 break
-            # sys.stdout.write(str(data))
-            # sys.stdout.flush()
-            print(data.decode())
-    writer = threading.Thread(target=writeall, args=(chan,))
-    writer.start()
+            chan.send(x)  # 发送一个字符
 
-    try:
-        while True:
-            d = sys.stdin.read(1)
-            if not d:
-                break
-            chan.send(d)
-    except EOFError:
-        # user hit ^Z or F6
-        pass
+finally:
+    # 重新设置终端属性,将终端状态还原
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, oldtty)
 
-shell = windows_shell(chan)
 chan.close()
 tran.close()
-"""
--- test.py
+++ test.py
@@ -1,13 +1,154 @@
-import paramiko
+# 类 Fabric 主机管理程序开发：
+# 1. 运行程序列出主机组或者主机列表
+# 2. 选择指定主机或主机组
+# 3. 选择让主机或者主机组执行命令或者向其传输文件（上传/下载）
+# 4. 充分使用多线程或多进程
+# 5. 不同主机的用户名密码、端口可以不同
+import paramiko,threading,pickle,os
 
-# transport = paramiko.Transport(('10.0.0.5', 1212))
-# transport.connect(username='root', password='huayiqiu')
+Base_path = os.path.dirname(os.path.abspath(__file__))
+config_path = Base_path +'/core/config'
+#主机配置信息管理
+list_host = [{'host':'192.168.117.236','port':22,'user':'ubuntu1','password':'ubuntu1','group':1},
+             {'host':'192.168.117.237','port':22,'user':'ubuntu2','password':'ubuntu2','group':1},
+             {'host':'192.168.117.239','port':22,'user':'ubuntu3','password':'ubuntu3','group':2},
+             {'host':'192.168.117.240','port':22,'user':'aaa','password':'bbb','group':2}]
+def pickle_dump(path,file):
+    #存入配置文件
+    with open(path,'wb') as f:
+        pickle.dump(file,f)
+#pickle_dump(config_path,list_host)
+def pickle_load(path):
+    #取出配置文件
+    with open(path,'rb') as f:
+        list_host_new = pickle.load(f)
+    return list_host_new
+# list_host_new = pickle_load(config_path)
+# print(list_host_new)
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('10.0.0.5',1212,username='root',password='huayiqiu')
 
-stdin, stdout, stderr = ssh.exec_command('df')
-for i in stdout:
-    print(i)
-ssh.close()
+class Paramiko_sshd(object):
+    #sshd类
+    def __init__(self,host,port,user,password):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+
+    def sshd_command(self,command):
+        #ssh远程主机执行命令，并返回结果
+        try:
+            ssh_client = paramiko.SSHClient()
+            ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh_client.connect(self.host,self.port,self.user,self.password)
+            std_in,std_out,std_err = ssh_client.exec_command(command)
+            print(self.host.center(30,'*'))
+            print(command.upper().center(30,'='))
+            for line in std_out:
+                print(line.strip('\n'))
+            ssh_client.close()
+        except Exception as e:
+            print(e)
+    def sshd_upload_file(self,server_path,local_path):
+        #上传文件
+        try:
+            t = paramiko.Transport((self.host,self.port))
+            t.connect(username=self.user,password=self.password)
+            sftp = paramiko.SFTPClient.from_transport(t)
+            sftp.put(local_path,server_path)
+            t.close()
+            print(self.host.center(30, '*'))
+            print('上传成功')
+        except Exception as e:
+            print(e)
+    def sshd_down_file(self,server_path,local_path):
+        #下载文件
+        try:
+            t = paramiko.Transport((self.host, self.port))
+            t.connect(username=self.user, password=self.password)
+            sftp = paramiko.SFTPClient.from_transport(t)
+            sftp.get(server_path, local_path)
+            t.close()
+            print(self.host.center(30, '*'))
+            print('下载成功')
+        except Exception as e:
+            print(e)
+def help():
+    #帮助信息
+    command_option = input('''
+    *************请选择***********
+    1.执行命令
+    2.上传文件
+    3.下载文件
+    4.返回
+    5.退出程序
+    ''').strip()
+    return command_option
+
+def review():
+    while 1:
+        #展示界面
+        list_host_new = list_host#pickle_load(config_path)
+        host_group_1 = []
+        host_group_2 = []
+        for line in list_host_new:
+            if line['group'] == 1:
+                host_group_1.append(line)
+            else:
+                host_group_2.append(line)
+        print('group-1'.center(30,'='))
+        for line_1 in host_group_1:
+            print('host:',line_1['host'],'port:',line_1['port'])
+        print('group-2'.center(30,'='))
+        for line_2 in host_group_2:
+            print('host:',line_2['host'],'port:',line_2['port'])
+
+        group_select = input('请输入您要操作的主机组：\033[1;35m group-1 \033[0m | \033[1;35m group-2 \033[0m '
+                             '或输入\033[1;35mexit\033[0m退出程序 ==>').strip()
+        if group_select in ['group-1','group-2']:
+            if group_select == 'group-1':     #增加中间变量，来确定是对组一操作还是对组二进行操作
+                group_sure = host_group_1
+            else:
+                group_sure = host_group_2
+            while 1:
+                command_option = help()
+                if command_option == '1':
+                    command = input('请输入执行的命令：').strip()
+                    for line_3 in group_sure:
+                        sshd = Paramiko_sshd(line_3['host'],line_3['port'],line_3['user'],line_3['password'])
+                        t = threading.Thread(target=sshd.sshd_command,args=(command,))
+                        t.start()
+                        t.join()
+
+                elif command_option == '2':
+                    server_path = input('请输入上传到的服务器目录：').strip()
+                    local_path = input('请输入本地文件目录：').strip()
+                    for line_3 in group_sure:
+                        sshd = Paramiko_sshd(line_3['host'],line_3['port'],line_3['user'],line_3['password'])
+                        t = threading.Thread(target=sshd.sshd_upload_file,args=(server_path,local_path,))
+                        t.start()
+                        t.join()
+
+                elif command_option == '3':
+                    server_path = input('请输入下载的服务器目录文件路径：').strip()
+                    local_path = input('请输入存放到本地的路径：').strip()
+                    for line_3 in group_sure:
+                        sshd = Paramiko_sshd(line_3['host'],line_3['port'],line_3['user'],line_3['password'])
+                        t = threading.Thread(target=sshd.sshd_down_file,args=(server_path,local_path,))
+                        t.start()
+                        t.join()
+                elif command_option == '4':
+                    break
+                elif command_option == '5':
+                    exit()
+                else:
+                    print('%s为非法命令，请重新输入！'%command_option)
+        elif group_select == 'exit':
+            break
+        else:
+            print('%s为非法命令，请重新输入！'%group_select)
+
+
+
+if __name__ =='__main__':
+    review()
