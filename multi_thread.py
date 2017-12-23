cec10eb8a88932a5003ecb21c02b07ba3bb51cf3
huayiqiu <284527552@qq.com>
2017-12-23 20:34:26 +0800

    <add> first version to use

--- multi_thread.py
+++ multi_thread.py
@@ -1,9 +1,9 @@
-import time,paramiko,threading,os
+import time,paramiko,threading,queue
 
 Hosts = {
-    "g1":[("10.0.0.3",1212),("10.0.0.4",1212,)],
-    "g2":[("10.0.0.5",1212,)],
-    "g3":[("10.0.0.6",1212,)],
+    "G1":[("10.0.0.3",1212),("10.0.0.4",1212),("10.0.0.5",1212,),("10.0.0.6",1212)],
+    "G2":[("10.0.0.5",1212)],
+    "G3":[("10.0.0.6",1212)]
 }
 
 
@@ -12,8 +12,7 @@ help_menu = '''\033[1;32;1m
 ----------------------------------------
 Please select/input according to prompt.
 1.select  select group/host
-2.put  upload files to selected hosts
-3.get  download files to selected hosts
+2.run   run the muti hosts command line
 ----------------------------------------\033[0m
 '''
 
@@ -31,55 +30,169 @@ def login():
         print("login failed.")
         return 0
 
+class multi_hosts(object):
 
-def put(choice):
-    print("hh",choice)
+    def __init__(self,host):
+        '''
+        import a list with tuple as items
+        :param host:
+        '''
+        self.lock = threading.Lock()
+        self.host = host
+        self.run()
 
+    def put(self,host,comm):
+        '''
+        send files
+        sftp.put('D:/New folder/oldboy.txt','/tmp/from_windows')
+        :param host:
+        :param comm:
+        :return:
+        '''
+        list_comm= comm.split()
+        tran = paramiko.Transport(host)
+        tran.connect(username='root', password='huayiqiu')
+        sftp = paramiko.SFTPClient.from_transport(tran)
+        try:
+            with self.lock:
+                sftp.put(list_comm[1], list_comm[2])
+                res = "\033[1;32;1mtask [put] is finished!\033[0m"
+                # self.lock.acquire()
+                print(res)
+        except Exception as e:
+            print(e)
+        finally:
+            # self.lock.release()
+            sftp.close()
+    def get(self,host,comm):
+        '''
+        recv files
+        sftp.get('D:/New folder/oldboy.txt','/tmp/from_windows')
+        :param host:
+        :param comm:
+        :return:
+        '''
+        list_comm = comm.split()
+        tran = paramiko.Transport(host)
+        tran.connect(username='root', password='huayiqiu')
+        sftp = paramiko.SFTPClient.from_transport(tran)
+        try:
+            with self.lock:
+                sftp.get(list_comm[1], list_comm[2])
+                res = "\033[1;32;1mtask [get] is finished!\033[0m"
+                # self.lock.acquire()
+                print(res)
+        except Exception as e:
+            print(e)
+        finally:
+            # self.lock.release()
+            sftp.close()
 
-def get(choice):
-    print("hh", choice)
+    def exec_comm(self,host,comm):
+        '''
+        exec ssh command
+        :param host:
+        :param comm:
+        :return:
+        '''
+        tran = paramiko.Transport(host)
+        tran.connect(username='root',password='huayiqiu')
+        ssh = paramiko.SSHClient()
+        ssh._transport = tran
+        try:
+            with self.lock:
+                stdin,stdout,stderr = ssh.exec_command(comm)
+                # self.lock.acquire()
+                print("\033[1;32;1mresult from host [%s]:\033[0m"%host[0])
+                for line in stderr:
+                    print(line)
+                for line in stdout:
+                    print(line)
+        except Exception as e:
+            print("error:",e)
+        finally:
+            # self.lock.release()
+            ssh.close()
+    # def print_line(self):
+    #     while True:
+    #         if not self.quene.empty():
+    #             item = self.quene.get()
+    #             if item is not list:
+    #                 print(item)
+    #             else:
+    #                 print(item[0])
+    #                 item.read()
 
-
-
+    def run(self,):
+        # t = threading.Thread(target=self.print_line, args=())
+        # t.start()
+        while True:
+            print("--------------------------------------------\n"
+                  "if you want to transfer files,use as follows:\n"
+                  "put  D:/linux/test.txt /data/test.txt\n"
+                  "get  /data/test.txt D:/linux/test.txt\n"
+                  "--------------------------------------------\n")
+            comm = input("command:").strip()
+            if comm.startswith("put"):
+                for h in self.host:
+                    t = threading.Thread(target=self.put,args=((h,comm)))
+                    t.start()
+            elif comm.startswith("get"):
+                for h in self.host:
+                    t = threading.Thread(target=self.get,args=((h,comm)))
+                    t.start()
+            elif comm == 'b':
+                break
+            else:
+                for h in self.host:
+                    t = threading.Thread(target=self.exec_comm,args=((h,comm)))
+                    t.start()
 
 
 if __name__ == '__main__':
 
     choice = [] # global var
+    trace = {}
     a = login()
     while a:
         print(help_menu)
-        inp = input("please input(1,2,3,b):")
+        inp = input("please input(1,2,b):")
         if inp == '1':
             while inp != 'b':
-                for group in Hosts.keys():  # print group
-                    print("[%s]\n" % group,end='----\n')
+                trace.clear()
+                for i,group in enumerate(Hosts.keys(),1):  # print group
+                    print("%s.%s\n" %(i,group))
+                    trace.setdefault(str(i),group)
                     # for host in Hosts[group]:  # print host
                     #     print(host)
                 inp = input("Input group:")
-                if inp in Hosts.keys():
-                    choice = Hosts[inp]
-                    trace = {}
+                if inp in trace:
+                    choice=Hosts[trace[inp]].copy()
+                    trace.clear()
                     for i,host in enumerate(choice,1):
                         print(i,host)
-                        trace.setdefault(str(i),host)
-                    inp = input("chose num:")
+                        trace[str(i)] = host
+                    inp = input("choose num:")
                     if inp in trace:
                         choice.clear()
                         choice.append(trace[inp])
+                        print("You have choose:", choice)
+                        break
+                    else:
+                        print("invalid input!")
+                else:
+                    print("invalid input!")
             else:
-                print("You have choose:", choice)
+                print("You have choose:")
+                for i,host in enumerate(choice,1):
+                    print(i,host)
 
         elif inp == '2':
             if choice:
-                get(choice)
+                multi = multi_hosts(choice)
             else:
                 print("Please select hosts!")
-        elif inp == '3':
-            if choice:
-                get(choice)
-            else:
-                print("Please select hosts!")
+
         elif inp == 'b':
             pass
         else:
